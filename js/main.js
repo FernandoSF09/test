42e05ea6240f54d48e06aa6cd471abd771eb8cd4
Tendo <tendof59@gmail.com>
2021-12-15 00:32:58 -0500

    prueba

---
+++ js/main.js
@@ -0,0 +1,49 @@
+window.sr = ScrollReveal();
+	sr.reveal('header',{
+		duration:2000,
+		
+	});
+	sr.reveal('.carousel',{
+		duration:2000,
+		
+	});
+
+	sr.reveal('.colegio',{
+		duration:2000,
+		
+	});
+	sr.reveal('.depa',{
+		duration:2000,
+		
+	});
+	sr.reveal('.fibro',{
+		duration:2000,
+		
+	});
+	sr.reveal('.derecha',{
+		duration:2000,
+		
+	});
+	sr.reveal('.izquierda',{
+		duration:2000,
+		
+	});
+	sr.reveal('.icono',{
+		duration:1500,
+		
+	});
+	sr.reveal('.primera_linea',{
+		duration:2000,
+	});
+	sr.reveal('.hogar',{
+		duration:1500,
+	
+	});
+	sr.reveal('.tienda',{
+		duration:1500,
+		
+	});
+	sr.reveal('.oficina',{
+		duration:1500,
+		
+	});
